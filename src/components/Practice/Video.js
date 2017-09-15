d9271fbc3ac7723685fcab80f5bc2d478b1250e9
Ashlyn Mitros <ashlyndmitros@gmail.com>
2017-09-15 17:20:47 -0600

    styled embedded video, trying responsiveness

--- src/components/Practice/Video.js
+++ src/components/Practice/Video.js
@@ -2,20 +2,33 @@ import React from 'react';
 import styled from 'styled-components';
 
 import wakeboard from '../../assets/wakeboard.m4v';
-import galaxy from '../../assets/throatGalaxy.jpg'
+import trees from '../../assets/trees.jpg'
 
 const Video = () => {
   return (
-    <div>
-      <Media controls poster={ galaxy }>
+    <Container>
+      <Media controls poster={ trees }>
         <source src={ wakeboard }/>
       </Media>
-    </div>
+      <Embedded width="560" height="315" src="https://www.youtube.com/embed/3yOYsMyuIFI?rel=0&amp;showinfo=0" frameborder="0" allowfullscreen></Embedded>
+    </Container>
   );
 };
 
 export default Video;
 
+const Container = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
 const Media = styled.video`
   height: 500px;
+  margin: 20px 0;
+`
+const Embedded = styled.iframe`
+  display: block;
+  @media (max-width: 730px){
+    width: 400px;
+  }
 `
